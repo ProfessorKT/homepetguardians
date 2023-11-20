dea7edfcb78f356966a9b473333ef0ac1ba64fc5
BartoszNitschke <s25771@pjwstk.edu.pl>
2023-11-20 18:58:28 +0100

    signup 1.1

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -9,22 +9,26 @@ export default function Signup() {
         <h1 className="text-[#04A777] font-semibold text-[36px] pt-[100px] md:pt-[32px]">
           Sign up
         </h1>
-        <form action="" className="flex flex-col w-[688px] mx-auto">
+        <form
+          action=""
+          className="flex flex-col w-full justify-center items-center"
+        >
           <input
             type="text"
             name="name"
             id="name"
             placeholder="Name"
-            className="md:mt-10 mt-16 py-3 px-3 font-light border-2 border-[#04A777] rounded-lg md:text-[24px]
-            text-[18px] placeholder-black focus:outline-none focus:box-shadow-none"
+            className="md:mt-10 mt-16 py-3 px-3 font-light border border-[#04A777] rounded-lg md:text-[24px]
+            text-[18px] placeholder-black
+            "
           />
           <input
             type="email"
             name="email"
             id="email"
             placeholder="Email Address"
-            className="mt-8 py-3 px-3 font-light border-2 border-[#04A777] rounded-lg md:text-[24px]
-            text-[18px] placeholder-black focus:outline-none focus:box-shadow-none"
+            className="mt-8 py-3 px-3 font-light border border-[#04A777] rounded-lg md:text-[24px]
+            text-[18px] placeholder-black"
           />
 
           <input
@@ -32,13 +36,14 @@ export default function Signup() {
             name="password"
             id="password"
             placeholder="Password "
-            className="mt-8 py-3 px-3 font-light border-2 border-[#04A777] rounded-lg md:text-[24px]
-            text-[18px] placeholder-black focus:outline-none focus:box-shadow-none"
+            className="mt-8 py-3 px-3 font-light border border-[#04A777] rounded-lg md:text-[24px]
+            text-[18px] placeholder-black"
           />
-
-          <button className="bg-[#04A777] md:w-[260px] w-[685px] md:mx-auto py-2 rounded-3xl md:mt-[80px] mt-8 text-white font-semibold text-[24px]">
-            Sign Up
-          </button>
+          <div className="flex content-center">
+            <button className="bg-[#04A777] md:w-[260px] w-[200px] md:mx-auto py-2 rounded-3xl md:mt-[80px] mt-8 text-white font-semibold text-[24px]">
+              Sign Up
+            </button>
+          </div>
 
           <div className="md:mx-auto flex ml-3 md:mt-8 mt-11 text-[16px]">
             <p className="pr-1">Already have an account?</p>
