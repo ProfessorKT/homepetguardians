686e71c7595f9c7c9dc1e102e58a12644a47b26e
Mikołaj Sławnikowski <mikolaj.slawnikowski@gmail.com>
2023-11-20 17:00:42 +0100

    Added inputs

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -5,10 +5,25 @@ export default function Login() {
   return (
     <>
       <RootLayout />
-      <div className="h-screen w-screen flex justify-center">
-        <p className="text-[#04A777] font-semibold text-[36px] pt-[88px]">Sing In</p>
+      <div className="h-screen w-screen flex flex-col items-center">
+        <p className="text-[#04A777] font-semibold text-[36px] pt-[88px]">Sign In</p>
+        <form className="pt-[88px] flex flex-col">
+          <input
+            type="text"
+            name="login"
+            id="login"
+            placeholder="Email or Phone Number"
+            className="w-[686px] h-[80px] text-[32px] placeholder-black border-[#04A777] border-[1.5px] rounded-[8px] mb-[36px] px-[14px]"
+          />
+          <input
+            type="text"
+            name="password"
+            id="password"
+            placeholder="Password"
+            className="w-[686px] h-[80px] text-[32px] placeholder-black border-[#04A777] border-[1.5px] rounded-[8px] px-[14px]"
+          />
+        </form>
       </div>
-      
     </>
   );
 }
