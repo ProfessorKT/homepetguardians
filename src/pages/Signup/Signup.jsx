8fa2fadc3ee7f2a51836d8785f8d5438f621ec60
kamiltomczuk <s24802@pjwstk.edu.pl>
2023-11-20 18:44:33 +0100

    removed outline

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -15,32 +15,25 @@ export default function Signup() {
             name="name"
             id="name"
             placeholder="Name"
-            className="md:mt-10 mt-16 py-3 px-3 font-light border border-[#04A777] rounded-lg md:text-[24px]
-            text-[18px] placeholder-black"
+            className="md:mt-10 mt-16 py-3 px-3 font-light border-2 border-[#04A777] rounded-lg md:text-[24px]
+            text-[18px] placeholder-black focus:outline-none focus:box-shadow-none"
           />
           <input
             type="email"
             name="email"
             id="email"
             placeholder="Email Address"
-            className="mt-8 py-3 px-3 font-light border border-[#04A777] rounded-lg md:text-[24px]
-            text-[18px] placeholder-black"
+            className="mt-8 py-3 px-3 font-light border-2 border-[#04A777] rounded-lg md:text-[24px]
+            text-[18px] placeholder-black focus:outline-none focus:box-shadow-none"
           />
-          <input
-            type="number"
-            name="phone_number"
-            id="phone_number"
-            placeholder="Phone Number"
-            className="mt-8 py-3 px-3 font-light border border-[#04A777] rounded-lg md:text-[24px]
-            text-[18px] placeholder-black"
-          />
+
           <input
             type="password"
             name="password"
             id="password"
             placeholder="Password "
-            className="mt-8 py-3 px-3 font-light border border-[#04A777] rounded-lg md:text-[24px]
-            text-[18px] placeholder-black"
+            className="mt-8 py-3 px-3 font-light border-2 border-[#04A777] rounded-lg md:text-[24px]
+            text-[18px] placeholder-black focus:outline-none focus:box-shadow-none"
           />
 
           <button className="bg-[#04A777] md:w-[260px] w-[685px] md:mx-auto py-2 rounded-3xl md:mt-[80px] mt-8 text-white font-semibold text-[24px]">
