f7c080bddb8076cdaed397e9b78322a4ae9ae9cc
kamiltomczuk <s24802@pjwstk.edu.pl>
2023-11-27 20:39:48 +0100

    addef w-full and h-screen

--- app/components/AboutUs.jsx
+++ app/components/AboutUs.jsx
@@ -4,42 +4,44 @@ import piesek from "../assets/piesek.png";
 
 const AboutUs = () => {
   return (
-<section name="about-us" className="grid gap-8 md:grid-cols-2 w-full h-screen">
-  <div className="md:col-span-1 mb-[200px] flex justify-center md:mt-[250px] order-last ">
-    <Image src={piesek} alt="" className=""/>
-  </div>
-  <div className="md:col-span-1 order-first md:order-last">
-    <div className="w-[170px] border-b-[4px] border-[#04A777] ml-[10px] md:ml-[-770px]">
-      <h1 className="text-[40px] font-medium">About us</h1>
-    </div>
-    <p className="font-bold text-[26px] mx-[10px] md:mt-40 mt-20">
-      Welcome to <span className="text-[#04A777]">HomePetGuardians</span>
-    </p>
-    <p className="text-[22px] mx-[10px]">
-      your trusted partner in professional pet care!
-    </p>
-    <p className="text-[22px] mx-[10px]">
-      At <span className="text-[#04A777]">HomePetGuardians</span>, we take pride
-      in our team of dedicated caregivers who understand the special bond you
-      share with your pets.
-    </p>
-    <p className="text-[22px] mx-[10px]">
-      Our experienced professionals are committed to providing exceptional care
-      for your furry family members in the comfort of their own homes.
-    </p>
-    <p className="text-[22px] mx-[10px]">
-      With a passion for animals and a focus on their well-being, our caregivers
-      ensure that your pets receive the attention, love, and specialized care
-      they deserve.
-    </p>
-    <p className="text-[22px] mx-[10px]">
-      Choose <span className="text-[#04A777]">HomePetGuardians</span> for peace
-      of mind, knowing that your pets are in the hands of skilled and
-      compassionate professionals who treat them like family.
-    </p>
-  </div>
-</section>
-
+    <section name="about-us" className=" w-full h-screen">
+      <div className="grid gap-8 md:grid-cols-2">
+        <div className="md:col-span-1 mb-[200px] flex justify-center md:mt-[250px] order-last ">
+          <Image src={piesek} alt="" className="" />
+        </div>
+        <div className=" md:col-span-1 order-first md:order-last">
+          <div className="w-[170px] border-b-[4px] border-[#04A777] ml-[10px] md:ml-[-770px]">
+            <h1 className="text-[40px] font-medium">About us</h1>
+          </div>
+          <p className="font-bold text-[26px] mx-[10px] md:mt-40 mt-20">
+            Welcome to <span className="text-[#04A777]">HomePetGuardians</span>
+          </p>
+          <p className="text-[22px] mx-[10px]">
+            your trusted partner in professional pet care!
+          </p>
+          <p className="text-[22px] mx-[10px]">
+            At <span className="text-[#04A777]">HomePetGuardians</span>, we take
+            pride in our team of dedicated caregivers who understand the special
+            bond you share with your pets.
+          </p>
+          <p className="text-[22px] mx-[10px]">
+            Our experienced professionals are committed to providing exceptional
+            care for your furry family members in the comfort of their own
+            homes.
+          </p>
+          <p className="text-[22px] mx-[10px]">
+            With a passion for animals and a focus on their well-being, our
+            caregivers ensure that your pets receive the attention, love, and
+            specialized care they deserve.
+          </p>
+          <p className="text-[22px] mx-[10px]">
+            Choose <span className="text-[#04A777]">HomePetGuardians</span> for
+            peace of mind, knowing that your pets are in the hands of skilled
+            and compassionate professionals who treat them like family.
+          </p>
+        </div>
+      </div>
+    </section>
   );
 };
 
--- app/components/HomeSection.jsx
+++ app/components/HomeSection.jsx
@@ -4,16 +4,22 @@ import piesek from "../assets/piesek.png";
 
 const HomeSection = () => {
   return (
-      <section id="home" className="grid gap-8 md:grid-cols-2">
-        
-        <div className="mt-[100px] mx-[10px] text-[62px] font-medium mb-[20px] text-lef md:ml-[200px] md:pt-[100px]">Does your pet need care while you are away?</div>
-        <div className="mx-[10px] text-[40px] mb-[20px] text-left md:col-span-2  md:pl-[200px] md:w-1/2">We offer various care options for your pupil.</div>
-        <button className="w-[350px]  px-[10px] py-[20px] text-[32px] bg-[#04A777] text-white rounded-full mx-auto md:h-[100px] md:w-[300px] md:ml-[200px]">Check our offer</button>
-       <div className="mb-[100px] mt-40 flex justify-center md:mt-[-400px]">
-       <Image src={piesek} alt="" className=""/>
-       </div>
-       
-     </section>
+    <section id="home" className="w-full h-screen">
+      <div className="grid gap-8 md:grid-cols-2">
+        <div className="mt-[100px] mx-[10px] text-[62px] font-medium mb-[20px] text-lef md:ml-[200px] md:pt-[100px]">
+          Does your pet need care while you are away?
+        </div>
+        <div className="mx-[10px] text-[40px] mb-[20px] text-left md:col-span-2  md:pl-[200px] md:w-1/2">
+          We offer various care options for your pupil.
+        </div>
+        <button className="w-[350px]  px-[10px] py-[20px] text-[32px] bg-[#04A777] text-white rounded-full mx-auto md:h-[100px] md:w-[300px] md:ml-[200px]">
+          Check our offer
+        </button>
+        <div className="mb-[100px] mt-40 flex justify-center md:mt-[-400px]">
+          <Image src={piesek} alt="" className="" />
+        </div>
+      </div>
+    </section>
   );
 };
 
