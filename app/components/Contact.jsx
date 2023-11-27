6cfc3f9605fc7e6bd9200b9252af2c884640561c
BartoszNitschke <s25771@pjwstk.edu.pl>
2023-11-27 16:14:19 +0100

    contact header

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -3,7 +3,15 @@ import React from "react";
 const Contact = () => {
   return (
     <div name="contact" className="w-full h-screen">
-      Contact
+      <div className="pt-[140px]">
+        <h1 className="text-[40px] font-medium w-[150px] border-b-[4px] border-[#04A777] border- ml-[200px]">
+          Contact
+        </h1>
+        <p className="ml-[200px] py-2 text-[20px]">
+          If you have any questions just ask us! We will be happy to answer all
+          of them.
+        </p>
+      </div>
     </div>
   );
 };
