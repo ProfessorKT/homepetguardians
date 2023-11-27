bd3d4555302c2f1c664b15a9681d0ba39dbb4f37
Szymonnowicki1 <92035422+Szymonnowicki1@users.noreply.github.com>
2023-11-27 16:15:41 +0100

    homeSection

--- app/components/HomeSection.jsx
+++ app/components/HomeSection.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import Image from "next/image";
+import piesek from "../assets/piesek.png";
 
 const HomeSection = () => {
   return (
-    <div name="home" className="w-full h-screen">
-      Home
-    </div>
+<section id="home" className="grid gap-8 md:grid-cols-2">
+        
+        <div className="mt-[100px] ml-[10px] mr-[10px] text-[62px] font-medium mb-[20px] text-lef md:pl-[100px] md:pt-[200px]">Does your pet need care while you are away?</div>
+        <div className="ml-[10px] mr-[10px] text-[40px] mb-[20px] text-left md:col-span-2 md:pl-[100px] md:w-1/2">We offer various care options for your pupil.</div>
+        <button className="w-[400px]  px-[10px] py-[20px] text-[32px] bg-[#04A777] text-white rounded-full mx-auto md:h-[100px] md:w-[300px] md:ml-[100px]">Check our offer</button>
+       <div className="mb-[10px] flex justify-center md:mt-[-400px]">
+       <Image src={piesek} alt="" className=""/>
+       </div>
+       
+     </section>
   );
 };
 
