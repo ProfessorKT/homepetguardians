a7e2fd4d28bea85b57d2728c593382b9b1861d2d
Szymonnowicki1 <92035422+Szymonnowicki1@users.noreply.github.com>
2023-11-27 18:22:00 +0100

    sekcja aboutus

--- app/components/HomeSection.jsx
+++ app/components/HomeSection.jsx
@@ -4,12 +4,12 @@ import piesek from "../assets/piesek.png";
 
 const HomeSection = () => {
   return (
-<section id="home" className="grid gap-8 md:grid-cols-2">
+      <section id="home" className="grid gap-8 md:grid-cols-2">
         
-        <div className="mt-[100px] ml-[10px] mr-[10px] text-[62px] font-medium mb-[20px] text-lef md:pl-[100px] md:pt-[200px]">Does your pet need care while you are away?</div>
-        <div className="ml-[10px] mr-[10px] text-[40px] mb-[20px] text-left md:col-span-2 md:pl-[100px] md:w-1/2">We offer various care options for your pupil.</div>
-        <button className="w-[400px]  px-[10px] py-[20px] text-[32px] bg-[#04A777] text-white rounded-full mx-auto md:h-[100px] md:w-[300px] md:ml-[110px]">Check our offer</button>
-       <div className="mb-[10px] flex justify-center md:mt-[-400px]">
+        <div className="mt-[100px] mx-[10px] text-[62px] font-medium mb-[20px] text-lef md:ml-[200px] md:pt-[100px]">Does your pet need care while you are away?</div>
+        <div className="mx-[10px] text-[40px] mb-[20px] text-left md:col-span-2  md:pl-[200px] md:w-1/2">We offer various care options for your pupil.</div>
+        <button className="w-[350px]  px-[10px] py-[20px] text-[32px] bg-[#04A777] text-white rounded-full mx-auto md:h-[100px] md:w-[300px] md:ml-[200px]">Check our offer</button>
+       <div className="mb-[100px] mt-40 flex justify-center md:mt-[-400px]">
        <Image src={piesek} alt="" className=""/>
        </div>
        
