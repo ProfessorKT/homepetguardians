66074a747479461d3e1e81ae9a599cd8f9646e15
Szymonnowicki1 <92035422+Szymonnowicki1@users.noreply.github.com>
2023-11-29 00:31:48 +0100

    responsywnosc mobilna i lekkie poprawki w about us

--- app/components/AboutUs.jsx
+++ app/components/AboutUs.jsx
@@ -2,37 +2,39 @@ import React from "react";
 import Image from "next/image";
 import piesek from "../assets/piesek.png";
 
+
 const AboutUs = () => {
   return (
 <section name="about-us" className="grid gap-8 md:grid-cols-2 w-full h-screen">
-  <div className="md:col-span-1 mb-[200px] flex justify-center md:mt-[250px] order-last ">
+  <div className="md:col-span-1 mb-[200px] flex justify-center md:mt-[150px] order-last ">
     <Image src={piesek} alt="" className=""/>
   </div>
+  <div className="ml-[10px] md:col-span-2 md:ml-[200px] border-b-[4px] border-[#04A777] w-[170px] order-first">
+    <h1 className="text-[40px] font-medium ">About us</h1>
+  </div>
+  
   <div className="md:col-span-1 order-first md:order-last">
-    <div className="w-[170px] border-b-[4px] border-[#04A777] ml-[10px] md:ml-[-770px]">
-      <h1 className="text-[40px] font-medium">About us</h1>
-    </div>
-    <p className="font-bold text-[26px] mx-[10px] md:mt-40 mt-20">
+    <p className="font-bold text-[26px] mx-[10px] md:mt-60 mt-20 md:text-right md:mr-[200px]">
       Welcome to <span className="text-[#04A777]">HomePetGuardians</span>
     </p>
-    <p className="text-[22px] mx-[10px]">
+    <p className="text-[22px] mx-[10px] md:text-right  md:mr-[200px]">
       your trusted partner in professional pet care!
     </p>
-    <p className="text-[22px] mx-[10px]">
+    <p className="text-[22px] mx-[10px] md:text-right  md:mr-[200px]">
       At <span className="text-[#04A777]">HomePetGuardians</span>, we take pride
       in our team of dedicated caregivers who understand the special bond you
       share with your pets.
     </p>
-    <p className="text-[22px] mx-[10px]">
+    <p className="text-[22px] mx-[10px] md:text-right  md:mr-[200px]">
       Our experienced professionals are committed to providing exceptional care
       for your furry family members in the comfort of their own homes.
     </p>
-    <p className="text-[22px] mx-[10px]">
+    <p className="text-[22px] mx-[10px] md:text-right  md:mr-[200px]">
       With a passion for animals and a focus on their well-being, our caregivers
       ensure that your pets receive the attention, love, and specialized care
       they deserve.
     </p>
-    <p className="text-[22px] mx-[10px]">
+    <p className="text-[22px] mx-[10px] md:text-right  md:mr-[200px]">
       Choose <span className="text-[#04A777]">HomePetGuardians</span> for peace
       of mind, knowing that your pets are in the hands of skilled and
       compassionate professionals who treat them like family.
--- app/components/HomeSection.jsx
+++ app/components/HomeSection.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Image from "next/image";
 import piesek from "../assets/piesek.png";
 
+
+
 const HomeSection = () => {
   return (
-      <section id="home" className="grid gap-8 md:grid-cols-2">
+      <section id="home" className="grid gap-8 md:grid-cols-2 w-full h-screen">
         
-        <div className="mt-[100px] mx-[10px] text-[62px] font-medium mb-[20px] text-lef md:ml-[200px] md:pt-[100px]">Does your pet need care while you are away?</div>
-        <div className="mx-[10px] text-[40px] mb-[20px] text-left md:col-span-2  md:pl-[200px] md:w-1/2">We offer various care options for your pupil.</div>
+        <div className="mt-[100px] mx-[10px] text-[62px] font-medium mb-[20px] text-lef md:ml-[200px] md:pt-[200px] md:mb-[30px]">Does your pet need care while you are away?</div>
+        <div className="mx-[10px] text-[40px] mb-[20px] text-left md:col-span-2  md:pl-[200px] md:w-1/2 md:mb-[30px]">We offer various care options for your pupil.</div>
         <button className="w-[350px]  px-[10px] py-[20px] text-[32px] bg-[#04A777] text-white rounded-full mx-auto md:h-[100px] md:w-[300px] md:ml-[200px]">Check our offer</button>
        <div className="mb-[100px] mt-40 flex justify-center md:mt-[-400px]">
        <Image src={piesek} alt="" className=""/>
