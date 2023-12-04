fcce54a37828f1009bc96273dea97f10f7a5c163
Mikołaj Sławnikowski <mikolaj.slawnikowski@gmail.com>
2023-12-04 22:46:17 +0100

    full responsive empty bento

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -3,105 +3,31 @@ import React from "react";
 const Services = () => {
   return (
     <div name="services" className="w-full h-screen">
-      <h1 className="text-[36px] font-medium border-b-[4px] border-[#04A777] w-[137px] ml-[8px] pt-[76px]">
+      <h1 className="text-[36px] font-medium border-b-[4px] border-[#04A777] w-[163px] ml-[8px] pt-[76px]">
         Services
       </h1>
-
-      <div className="2xl:hidden grid grid-cols-2 w-[352px] gap-x-[9px] gap-y-[8px] mx-auto pt-[43px]">
+      <div className="flex justify-center mt-[24px]">
         <div
-          name="uwu1"
-          className="bg-gradient-to-r from-[#F1DABF] to-[#E4CDB1] rounded-[32px] w-[100] h-[130px] flex items-center justify-center"
+          name="bento"
+          className="grid md:grid-cols-4 md:grid-rows-3 w-full md:max-w-[800px] md:h-[560px] lg:max-w-[1106px] lg:h-[752px] xl:max-w-[1106px] 2xl:max-w-[1408px] mx-8 gap-x-[8px] xl:gap-x-[16px] gap-y-[8px] xl:gap-y-[16px]"
         >
-          grafika
-        </div>
-        <div
-          name="uwu1"
-          className="bg-gradient-to-r from-[#F1DABF] to-[#E4CDB1] rounded-[32px] w-[100] h-[130px] flex items-center justify-center"
-        >
-          3
-        </div>
-        <div
-          name="uwu1"
-          className="bg-gradient-to-r from-[#F1DABF] to-[#E4CDB1] rounded-[32px] w-[100] h-[130px] flex items-center justify-center col-span-2"
-        >
-          kot
-        </div>
-        <div
-          name="uwu1"
-          className="bg-gradient-to-r from-[#F1DABF] to-[#E4CDB1] rounded-[32px] w-[100] h-[130px] flex items-center justify-center"
-        >
-          10
-        </div>
-        <div
-          name="uwu1"
-          className="bg-gradient-to-r from-[#F1DABF] to-[#E4CDB1] rounded-[32px] w-[100] h-[130px] flex items-center justify-center"
-        >
-          5
-        </div>
-        <div
-          name="uwu1"
-          className="bg-gradient-to-r from-[#F1DABF] to-[#E4CDB1] rounded-[32px] w-[100] h-[130px] flex items-center justify-center col-span-2"
-        >
-          food
-        </div>
-      </div>
-      <div className="hidden 2xl:flex h-screen justify-center mx-4 mt-[267px]">
-        <div className="w-[980px] h-[648px] grid grid-cols-2 gap-x-[24px] gap-y-[24px] mr-[12px]">
-          <div
-            name="siema4"
-            className="bg-gradient-to-r from-[#F1DABF] to-[#E4CDB1] rounded-[32px] w-[340px] h-[240px] flex items-center justify-center"
-          >
-            grafika
+          <div className="w-full h-[190px] md:h-full bg-gradient-to-r from-[#F1DABF] to-[#E4CDB1] rounded-[32px] flex items-center justify-center col-span-2 row-span-2">
+            1
           </div>
-          <div
-            name="siema5"
-            className="bg-gradient-to-r from-[#F1DABF] to-[#E4CDB1] rounded-[32px] w-[480px] h-[240px] flex items-center justify-center ml-[-140px]"
-          >
-            10
+          <div className="w-full h-[130px] md:h-full bg-gradient-to-r from-[#F1DABF] to-[#E4CDB1] rounded-[32px] flex items-center justify-center">
+            2
           </div>
-          <div
-            name="siema6"
-            className="bg-gradient-to-r from-[#F1DABF] to-[#E4CDB1] rounded-[32px] w-[480px] h-[285px] flex items-center justify-center"
-          >
-            food
-          </div>
-          <div
-            name="siema7"
-            className="bg-gradient-to-r from-[#F1DABF] to-[#E4CDB1] rounded-[32px] w-[340px] h-[285px] flex items-center justify-center"
-          >
-            logo
-          </div>
-          <div
-            name="siema8"
-            className="bg-gradient-to-r from-[#F1DABF] to-[#E4CDB1] rounded-[32px] w-[480px] h-[75px] flex items-center justify-center"
-          >
-            Socializing with other pupils
-          </div>
-          <div
-            name="siema9"
-            className="bg-gradient-to-r from-[#F1DABF] to-[#E4CDB1] rounded-[32px] w-[340px] h-[75px] flex items-center justify-center"
-          >
-            99/100 pets liked this
-          </div>
-        </div>
-        <div className="w-[704px] h-[648px] grid grid-cols-2 gap-x-[24px] gap-y-[24px] 2xl:ml-[-128px] mt-8 2xl:mt-0">
-          <div
-            name="siema1"
-            className="bg-gradient-to-r from-[#F1DABF] to-[#E4CDB1] rounded-[32px] w-100 h-[240px] flex items-center justify-center"
-          >
-            5
-          </div>
-          <div
-            name="siema2"
-            className="bg-gradient-to-r from-[#F1DABF] to-[#E4CDB1] rounded-[32px] h-[240px] flex items-center justify-center"
-          >
+          <div className="w-full h-[130px] md:h-full bg-gradient-to-r from-[#F1DABF] to-[#E4CDB1] rounded-[32px] flex items-center justify-center">
             3
           </div>
-          <div
-            name="siema3"
-            className="bg-gradient-to-r from-[#F1DABF] to-[#E4CDB1] rounded-[32px] col-span-2 h-[384px] flex items-center justify-center"
-          >
-            kot
+          <div className="w-full h-[190px] md:h-full bg-gradient-to-r from-[#F1DABF] to-[#E4CDB1] rounded-[32px] flex items-center justify-center col-span-2 row-span-2">
+            4
+          </div>
+          <div className="w-full h-[130px] md:h-full bg-gradient-to-r from-[#F1DABF] to-[#E4CDB1] rounded-[32px] flex items-center justify-center">
+            5
+          </div>
+          <div className="w-full h-[130px] md:h-full bg-gradient-to-r from-[#F1DABF] to-[#E4CDB1] rounded-[32px] flex items-center justify-center">
+            6
           </div>
         </div>
       </div>
