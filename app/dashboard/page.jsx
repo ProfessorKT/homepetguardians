990d1e21df759519204de155ee62ba138912099f
kamiltomczuk <s24802@pjwstk.edu.pl>
2023-12-06 00:01:16 +0100

    configured firebase for project

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,9 +1,34 @@
+"use client";
 import React from "react";
+import { useAuth } from "@clerk/nextjs";
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithCustomToken } from "firebase/auth";
+import { useEffect } from "react";
+import firebaseConfig from "../../lib/firebase.config";
+
+initializeApp(firebaseConfig);
 
 const DashboardPage = () => {
+  const { getToken } = useAuth();
+
+  useEffect(() => {
+    const signInWithClerk = async () => {
+      const auth = getAuth();
+      const token = await getToken({ template: "integration_firebase" });
+      const userCredentials = await signInWithCustomToken(auth, token);
+
+      /**
+       * The userCredentials.user object will call the methods of
+       * the Firebase platform as an authenticated user.
+       */
+      console.log("user ::", userCredentials.user);
+    };
+
+    signInWithClerk();
+  }, []);
   return (
     <>
-      <div> DashboardPage</div>
+      <div> DashboardPage </div>
     </>
   );
 };
