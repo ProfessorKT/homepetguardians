cfbf0b772072457c2675f4bffff166570a05baaf
kamiltomczuk <s24802@pjwstk.edu.pl>
2023-12-14 15:19:48 +0100

    Added hover button on HomeSection

--- app/components/HomeSection.jsx
+++ app/components/HomeSection.jsx
@@ -21,9 +21,28 @@ const HomeSection = () => {
             <div className="mt-12 mb-12 lg:mb-0 lg:flex lg:w-[350px] w-full grid place-content-center lg:place-content-start">
               <Link
                 href="/dashboard"
-                className="w-[300px] py-[20px] text-2xl bg-[#04A777] text-white rounded-[32px] justify-center items-center align-middle text-center"
+                className="relative inline-flex items-center justify-center w-[250px] h-[60px] p-4 px-6 py-3 overflow-hidden font-medium text-jade transition duration-300 ease-out border-2 border-jade rounded-full shadow-md group"
               >
-                Check our offer
+                <span class="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-jade group-hover:translate-x-0 ease">
+                  <svg
+                    class="w-6 h-6"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M14 5l7 7m0 0l-7 7m7-7H3"
+                    ></path>
+                  </svg>
+                </span>
+                <span class="absolute flex items-center justify-center w-full h-full text-jade transition-all duration-300 transform group-hover:translate-x-full ease">
+                  Check our offer
+                </span>
+                <span class="relative invisible">Check our offer</span>
               </Link>
             </div>
           </div>
