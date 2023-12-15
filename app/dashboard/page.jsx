927ef0e1a4dfb4d2cd24b05b149a7ef62789779d
BartoszNitschke <s25771@pjwstk.edu.pl>
2023-12-15 16:59:05 +0100

    Revert "moved error to console"
    This reverts commit c65ff1f3ac7c68f9f03ccee1e9c53ba9bc3b527b.

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -5,7 +5,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth, signInWithCustomToken } from "firebase/auth";
 import { useEffect } from "react";
 import firebaseConfig from "../../lib/firebase.config";
-import { SignedIn, SignedOut } from "@clerk/nextjs";
 
 // initializeApp(firebaseConfig);
 
@@ -16,31 +15,19 @@ const DashboardPage = () => {
     const signInWithClerk = async () => {
       const auth = getAuth();
       const token = await getToken({ template: "integration_firebase" });
-      try {
-        const userCredentials = await signInWithCustomToken(auth, token);
-        console.log("user ::", userCredentials.user);
-      } catch (error) {
-        console.error("Błąd uwierzytelniania:", error.code, error.message);
-      }
+      const userCredentials = await signInWithCustomToken(auth, token);
 
       /**
        * The userCredentials.user object will call the methods of
        * the Firebase platform as an authenticated user.
        */
+      console.log("user ::", userCredentials.user);
     };
 
     signInWithClerk();
   }, []);
   return (
     <>
-      <SignedIn>
-        {/* Div for debug purposes, not needed later on, SignedIn works even if logged via SignedOut option which causes error in the console */}
-        <div className="bg-red-500 h-screen"></div>
-      </SignedIn>
-      <SignedOut>
-        {/* Div for debug purposes, never shows up, if logged out redirection happens */}
-        <div className="bg-blue-500 h-screen"></div>
-      </SignedOut>
       <div> DashboardPage </div>
     </>
   );
