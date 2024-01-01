8e0965f62b62111bfabfc1e22ae85e5bea8e3350
kamiltomczuk <s24802@pjwstk.edu.pl>
2024-01-01 18:13:23 +0100

    Created a form for user dashboard
    Created a form for user dashboard which fetches cities, and pets from firestore database, also added calendar for date range for user. Stills needs to do the action for this form.

--- app/components/DashboardSelectForm.jsx
+++ app/components/DashboardSelectForm.jsx
@@ -1,6 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import firebase from "firebase/app";
+import { db } from "../../lib/firebase.config.js";
+import "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const DashboardSelectForm = () => {
+  const [cities, setCities] = useState([]);
+  const [pets, setPets] = useState([]);
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+
+  const formatDateRange = (start, end) => {
+    if (!start || !end) return "Choose date range";
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    return `${start.toLocaleDateString(
+      "en-US",
+      options
+    )} - ${end.toLocaleDateString("en-US", options)}`;
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const cityData = await getDocs(collection(db, "Cities"));
+      const petData = await getDocs(collection(db, "Pets"));
+
+      // console.log("cityData", cityData);
+      // console.log("petData", petData);
+
+      const cityArray = cityData.docs.map((doc) => doc.data().city);
+      const petArray = petData.docs.map((doc) => doc.data().pet);
+
+      // console.log("cityArray", cityArray);
+      // console.log("petArray", petArray);
+
+      setCities(cityArray.flat());
+      setPets(petArray.flat());
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="w-full h-screen">
       <div className="flex justify-center pt-[100px]">
@@ -16,35 +57,46 @@ const DashboardSelectForm = () => {
             id=""
             className="w-[450px] my-5 mx-auto border-solid border-[1px] rounded-xl p-1 bg-almond border-jade"
           >
-            <option value="">Chosee location</option>
-            <option value="">Warsaw</option>
-            <option value="">Gdansk</option>
-            <option value="">Krakow</option>
+            <option value="">Choose city</option>
+            {cities.map((city, index) => (
+              <option key={index} value={city}>
+                {city}
+              </option>
+            ))}
           </select>
           <select
             name=""
             id=""
             className="w-[450px] my-5 mx-auto border-solid border-[1px] rounded-xl p-1 bg-almond border-jade"
           >
-            <option value="">Chosee pet</option>
-            <option value="">Dog</option>
-            <option value="">Cat</option>
-            <option value="">Bird</option>
-            <option value="">Rodent</option>
+            <option value="">Choose pet</option>
+            {pets.map((pet, index) => (
+              <option key={index} value={pet}>
+                {pet}
+              </option>
+            ))}
           </select>
-          <select
-            name=""
-            id=""
+          <input
+            type="text"
+            value={formatDateRange(startDate, endDate)}
+            readOnly
             className="w-[450px] my-5 mx-auto border-solid border-[1px] rounded-xl p-1 bg-almond border-jade"
-          >
-            <option value="">Chosee date</option>
-            <option value="">Calendar here</option>
-            <option value="">Calendar here</option>
-            <option value="">Calendar here</option>
-          </select>
-          {/* <button className="w-[200px] mx-auto my-4 py-2 bg-jade rounded-2xl cursor-pointer text-snow-white">
-            Look for petsitter
-          </button> */}
+          />
+          <div className="flex justify-center">
+            <DatePicker
+              selected={startDate}
+              onChange={(dates) => {
+                const [start, end] = dates;
+                setStartDate(start);
+                setEndDate(end);
+              }}
+              startDate={startDate}
+              endDate={endDate}
+              selectsRange
+              inline
+              className=""
+            />
+          </div>
           <a
             href="#_"
             class=" w-[200px] mx-auto text-center rounded-2xl px-5 py-2.5 my-4 overflow-hidden group bg-jade relative hover:bg-gradient-to-r hover:from-jade hover:lightTurquoiseColor text-snow-white hover:ring-2 hover:ring-offset-2 hover:ring-green-400 transition-all ease-out duration-300"
